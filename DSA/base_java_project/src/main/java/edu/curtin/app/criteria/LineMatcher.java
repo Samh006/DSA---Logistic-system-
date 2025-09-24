a9c644f7120b3f070cdaf92ae676de4b49013585
Sam <mohammadsam.hassanpour@gmail.com>
2025-09-24 16:39:09 +0800

    Delete DSA directory

--- DSA/base_java_project/src/main/java/edu/curtin/app/criteria/LineMatcher.java
+++
@@ -1,7 +0,0 @@
-package edu.curtin.app.criteria;
-
-/* interface for matching a line against a condition */
-public interface LineMatcher {
-    boolean matches(String line);        // checks if a given line matches the condition, true if yes, false if no
-    
-}
